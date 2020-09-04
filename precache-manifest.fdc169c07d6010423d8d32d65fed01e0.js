dcc0010822ae785957d86c7f4150db110da32f09
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-09-04 15:27:29 +0300

    Updates

---
+++ precache-manifest.fdc169c07d6010423d8d32d65fed01e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "182f9d8c1fc33277bf03707f6c822846",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1dcf18e269f93c19980b",
+    "url": "./static/css/main.f4c9d8cd.chunk.css"
+  },
+  {
+    "revision": "368e64162689e816031d",
+    "url": "./static/js/2.0aa0400f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.0aa0400f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dcf18e269f93c19980b",
+    "url": "./static/js/main.3ad742ca.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
